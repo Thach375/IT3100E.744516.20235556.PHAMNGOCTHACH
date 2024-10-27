e7564e7018b8d2a241091e21d002775252ea7d66
Thach375 <phamthach03072005@gmail.com>
2024-10-27 10:51:11 +0700

    final

---
+++ LAB_2/ReadingAsignment/Aims/Cart.java
@@ -0,0 +1,44 @@
+package Aims;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private final DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+	private int qtyOrdered = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+			System.out.print("Disc has been add");
+		}
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		boolean found = false;
+		for (int i = 0; i < qtyOrdered; i++) {
+			if (itemOrdered[i] == disc) {
+				found = true;
+				
+				for (int j=i; j < qtyOrdered; j++) {
+					itemOrdered[j] = itemOrdered[j+1];
+				}
+				itemOrdered[qtyOrdered-1] = null;
+				qtyOrdered--;
+				System.out.println("disc " + disc.getTitle() + " has been removered");
+				break;
+			}
+		}
+		if (!found) {
+			System.out.println("Cannot found disc");
+		}
+	}
+	
+	public float totalCost() {
+		float sum = 0.0f;
+		for (int i=0; i < qtyOrdered; i++) {
+			sum += itemOrdered[i].getCost();
+		}
+		return sum;
+	}
+}
