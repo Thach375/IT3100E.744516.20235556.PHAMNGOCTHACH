202e6f3c39422e0444203411a553a69c7868cebf
Thach375 <phamthach03072005@gmail.com>
2024-11-28 14:36:58 +0700

    today commit

---
+++ AimsProject/hust/soict/dsai/aims/media/Book.java
@@ -0,0 +1,24 @@
+package AimsProject.hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public Book(int id, String title, String category, float cost, List<String> authors) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.authors = authors;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
